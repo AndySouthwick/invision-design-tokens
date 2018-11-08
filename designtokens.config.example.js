0ecf23a171d5b9ce1dfb047d7666cc869543ffa5
Andrew Southwick <Andrew.Southwick@us.usana.com>
2018-11-07 20:54:52 -0700

    added icon pull

--- designtokens.config.example.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  {"url": "<YOUR INVISION DSM SASS URL>",
-    "location": "node_modules/invision-design-tokens/style-params.scss"},
-  {"url": "<YOUR INVISION DSM LESS URL>",
-    "location": "node_modules/invision-design-tokens/style-params.less"},
-  {"url": "https://projects.invisionapp.com/dsm-export/usana-dsm/usana-design-system/style-params.styl?key=Sye_KlJfCf",
-    "location": "<YOUR INVISION DSM STYLUS URL>"}
-]
--- index.js
+++ index.js
@@ -2,16 +2,49 @@
 // import fs from 'fs'
 const fs = require('fs')
 const rp = require('request-promise')
-const invisionData = require('designtokens.config.example.js')
+const request = require('request')
+const invisionData = require('../../designtokens.config.js')
+const Zip = require('adm-zip');
+
+
+
+
 renderStyles = async (dataArray) => {
+  try{
   await dataArray.map(async data => {
-    const e = await rp(data.url)
-    let options = {encoding: 'UTF8'};
-    let writeStream = fs.createWriteStream(data.location, options);
-    writeStream.write(String(e))
-    writeStream.on('finish', () => {
-    })
+    if(data.location === 'icons.zip'){
+      await request(data.url).pipe(fs.createWriteStream(data.location)).on('close', function () {
+          renderIcons()
+      });
+    }
+      let e = await rp(data.url)
+      let options = {encoding: 'UTF8'};
+      let writeStream = fs.createWriteStream(data.location, options);
+      writeStream.write(String(e))
+      writeStream.on('finish', () => {
+      })
+
   })
+  }catch(e){
+    console.log(e)
+  }
+}
+
+
+ renderIcons = async () => {
+
+  try{
+    let zip = new Zip("icons.zip");
+    // get all entries and iterate them
+
+    zip.getEntries().forEach(function() {
+      zip.extractAllTo("./icons", true)
+    })
+  }catch(e){
+    console.log('the zip file came back bad please run again', e)
+  }
+
+
 }
 
 renderStyles(invisionData)
