b843681f602b6a329e1776d19551cae7c8e472c5
Andrew Southwick <Andrew.Southwick@us.usana.com>
2018-11-07 22:23:09 -0700

    bug fixes testing on spa

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// import rp from 'request-promise'
-// import fs from 'fs'
 const fs = require('fs')
 const rp = require('request-promise')
 const request = require('request')
-const invisionData = require('../../designtokens.config.js')
+const invisionData = require('./../../designtokens.config.js')
 const Zip = require('adm-zip');
 
 
@@ -11,12 +9,12 @@ const Zip = require('adm-zip');
 
 renderStyles = async (dataArray) => {
   try{
-  await dataArray.map(async data => {
-    if(data.location === 'icons.zip'){
-      await request(data.url).pipe(fs.createWriteStream(data.location)).on('close', function () {
+    await dataArray.map(async data => {
+      if(data.location === 'node_modules/invision-design-tokens/icons.zip'){
+        await request(data.url).pipe(fs.createWriteStream("icons.zip")).on('close', function () {
           renderIcons()
-      });
-    }
+        });
+      }
       let e = await rp(data.url)
       let options = {encoding: 'UTF8'};
       let writeStream = fs.createWriteStream(data.location, options);
@@ -24,21 +22,21 @@ renderStyles = async (dataArray) => {
       writeStream.on('finish', () => {
       })
 
-  })
+    })
   }catch(e){
     console.log(e)
   }
 }
 
 
- renderIcons = async () => {
+renderIcons = async () => {
 
   try{
     let zip = new Zip("icons.zip");
     // get all entries and iterate them
 
     zip.getEntries().forEach(function() {
-      zip.extractAllTo("./icons", true)
+      zip.extractAllTo("node_modules/invision-design-tokens/icons", true)
     })
   }catch(e){
     console.log('the zip file came back bad please run again', e)
