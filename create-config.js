d1e3a6c81a82ed8bc2c4943f4b785c53bcd09efe
Andrew Southwick <Andrew.Southwick@us.usana.com>
2018-11-09 16:14:37 -0700

    updated cli component to include update args

--- create-config.js
+++ create-config.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 const[,, ...args] = process.argv
 
-
+// section
 const createWriteToConfig = (data) => {
     console.log('from createWriteToConfig', data)
     let writeDataExports = `module.exports = [${data}]`
@@ -11,6 +11,10 @@ const createWriteToConfig = (data) => {
       console.log("successfully created config file")
     })
 }
+// section
+
+
+
 
 appendToConfigObject = () => {
   const invisionData = require('./designtokens.config')
@@ -20,16 +24,51 @@ appendToConfigObject = () => {
       let data1 = {"url": args[1]}
       let combinedArgs1 = Object.assign(data1, appendData)
       console.log(combinedArgs1)
-      createWriteToConfig(JSON.stringify(combinedArgs1))
+      if(appendData.url) {
+        checkForDataRewrite('url')
+      }else{
+        createWriteToConfig(JSON.stringify(combinedArgs1))
+      }
       break;
     case 'location':
       let data2 = {"location": args[1]}
       let combinedArgs2 = Object.assign(data2, appendData)
       console.log('stringified obj', JSON.stringify(combinedArgs2))
-      createWriteToConfig(JSON.stringify(combinedArgs2))
+      if(appendData.location) {
+        checkForDataRewrite('location')
+      }else{
+        createWriteToConfig(JSON.stringify(combinedArgs2))
+      }
+
+  }
+}
+// section
+
+
+checkForDataRewrite = (key) => {
+  const invisionData = require('./designtokens.config')
+  let appendData = invisionData[0]
+  if(key === 'url'){
+    let updatedData = {"url": args[1], "location": appendData.location}
+    let writeDataExports = `module.exports = [${JSON.stringify(updatedData)}]`
+    fs.writeFile('node_modules/invision-design-tokens/designtokens.config.js',  writeDataExports, (err) => {
+      if(err) log(err);
+      console.log("successfully updated config file")
+    })
+  }
+  if(key === 'location'){
+    let updatedData = {"location": args[1], "url": appendData.url}
+    let writeDataExports = `module.exports = [${JSON.stringify(updatedData)}]`
+    fs.writeFile('node_modules/invision-design-tokens/designtokens.config.js',  writeDataExports, (err) => {
+      if(err) log(err);
+      console.log("successfully updated config file")
+    })
   }
 }
 
+
+
+// section
 if(args[0] !== 'iconsUrl') {
   if (fs.existsSync('node_modules/invision-design-tokens/designtokens.config.js')) {
     appendToConfigObject()
@@ -44,4 +83,6 @@ if(args[0] !== 'iconsUrl') {
         createWriteToConfig(data2)
     }
   }
+}else{
+
 }
